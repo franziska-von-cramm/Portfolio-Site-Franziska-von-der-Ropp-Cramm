b8606e721f4d5ecf93d0789b411353de0c90bd18
franziska-von-cramm <Franziska.von.Cramm@gmail.com>
2023-10-11 13:51:14 +0200

    Update script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-console.log("UI/UX Design Portfolio")
+
+function toggleNavMenu() {
+    var toggledClassName = "open-nav"
+    var navElement = document.querySelector("nav ul")
+    var iconElement = document.querySelector(".hamburger")
+    console.log({ toggledClassName, navElement })
+    if (navElement.classList.contains(toggledClassName)) {
+        navElement.classList.remove(toggledClassName)
+        iconElement.classList.remove("fa-xmark")
+        iconElement.classList.add("fa-bars")
+    }
+    else {
+        navElement.classList.add(toggledClassName)
+        iconElement.classList.remove("fa-bars")
+        iconElement.classList.add("fa-xmark")
+    }
+}
